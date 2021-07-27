b824b6aa30c759be6d35821746891f423cfc88c1
lafler3 <lafler3@rpi.edu>
2021-07-27 16:26:46 -0400

    Back to the drawing board

--- project.c
+++ project.c
@@ -1,1 +1,26 @@
 //Project File Opsys
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+int next_exp(){
+	//had to restart
+}
+
+int main(int argc, char * argv[]){
+	int simN = argv[1];
+	int seedR = argv[2];
+	double lambda = argv[3];
+	int threshED = argv[4];
+	double conSwitch = argv[5];
+	double alphC = argv[6];
+	int timeSlice = argv[7];
+
+
+
+}
