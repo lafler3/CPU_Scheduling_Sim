89b270be11da23fb0080e016ba9e96fb6d49f9b2
Sullivanaidenaw <sullivanaidenaw@gmail.com>
2021-07-29 14:49:46 -0400

    Added deep copy function, printer functions, and converted main arguments

--- project.c
+++ project.c
@@ -44,6 +44,97 @@ int *** next_exp(double lambda, int simN, int threshED){
 	}
 	return data;
 }
+
+int*** deepCopy(int*** arr, int simN){
+	int burstN;
+	int aT;
+	int*** data = calloc(simN, sizeof(int**));
+	for (int i = 0; i < simN; ++i)
+	{
+		int* aBDC = calloc(3, sizeof(int));
+
+		burstN = arr[i][0][1];
+		aT = arr[i][0][0];
+		
+		*(aBDC + 0) = aT;
+		*(aBDC + 1) = burstN;
+		*(aBDC + 2) = 0;
+
+		int* tCPUC = calloc(burstN, sizeof(int));
+		for (int b = 0; b < burstN; ++b)
+		{
+			tCPUC[b] = arr[i][1][b];
+		}
+
+		int* tIOC = calloc((burstN-1), sizeof(int));
+		for (int b = 0; b < burstN - 1; ++b)
+		{
+			tIOC[b] = arr[i][2][b];
+		}
+
+		int** tArrayC = calloc(3, sizeof(int*));
+
+		*(tArrayC+0) = aBDC;
+		*(tArrayC+1) = tCPUC;
+		*(tArrayC+2) = tIOC;
+		*(data+i) = tArrayC;
+	}
+
+	return data;
+
+}
+
+void printData(int*** data, int simN){
+	for (int i = 0; i < simN; ++i)
+	{
+		printf("%d, %d, %d \n", data[i][0][0], data[i][0][1], data[i][0][2]);
+		int num = data[i][0][1];
+
+		for (int b = 0; b < num; ++b)
+		{
+			printf(" %d\n", data[i][1][b]);
+		}
+		for (int b = 0; b < num - 1; ++b)
+		{
+			printf(" %d\n", data[i][2][b]);
+		}
+	}
+}
+
+void printDataCompare(int*** data1, int*** data2, int simN){
+	for (int i = 0; i < simN; ++i)
+	{
+		printf("%d, %d, %d \n", data1[i][0][0], data1[i][0][1], data1[i][0][2]);
+		printf("%d, %d, %d \n", data2[i][0][0], data2[i][0][1], data2[i][0][2]);
+		int num = data1[i][0][1];
+
+		for (int b = 0; b < num; ++b)
+		{
+			printf(" %d\n", data1[i][1][b]);
+			printf(" %d\n", data2[i][1][b]);
+		}
+		for (int b = 0; b < num - 1; ++b)
+		{
+			printf(" %d\n", data1[i][2][b]);
+			printf(" %d\n", data2[i][2][b]);
+		}
+	}
+}
+
+int avg(double total, double processes){
+	int ret = ceil(total/processes);
+	return ret;
+}
+
+void sjf(int*** data, double conSwitch, double alphC){
+	double total = ceil(1/alphC);
+	double processes = 1;
+	int tau = avg(total, processes);
+
+}
+
+
+
 /*
 next_exp index: data[][][]
 data[] determines which process: data[0] = A, data[25] = Z
@@ -53,13 +144,14 @@ data[][1] = CPU burst times IN ORDER, data[][2] = I/O add times IN ORDER
 data[][][] refers to each indivisual value
 */
 int main(int argc, char * argv[]){
-	int simN = argv[1];
-	int seedR = argv[2];
-	double lambda = argv[3];
-	int threshED = argv[4];
-	double conSwitch = argv[5];
-	double alphC = argv[6];
-	int timeSlice = argv[7];
+	int simN = atoi(argv[1]);
+	int seedR = atoi(argv[2]);
+	double lambda = strtod(argv[3], NULL);
+	int threshED = atoi(argv[4]);
+	int conSwitch = atoi(argv[5]);
+	double alphC = strtod(argv[6], NULL);
+	int timeSlice = atoi(argv[7]);
+
 
 	srand(seedR);
 	//FCFS
@@ -68,6 +160,7 @@ int main(int argc, char * argv[]){
 	srand(seedR);
 	//SJF;
 	int*** SJFD = next_exp(lambda, simN, threshED);
+	int*** copy = deepCopy(SJFD,simN);
 
 	srand(seedR);
 	//SRT;
